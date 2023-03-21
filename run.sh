09fef1c8533b01c1142622d6274b56428b150acc
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2023-03-21 10:15:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,13 +9,6 @@ chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
 nohup /usr/sbin/sshd -D &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
-mkdir -p /root/tail
-cd /root/tail
-wget https://pkgs.tailscale.com/stable/tailscale_1.34.1_amd64.tgz
-tar xzf tailscale_1.34.1_amd64.tgz --strip-components=1
-mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
-nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-./tailscale up --authkey=tskey-auth-kFz69C5CNTRL-rXBP3XXvmoYtTV6SW3SftYDaQczMSmk8a --hostname=northf-vps
 
 cd /
 chmod +x server
